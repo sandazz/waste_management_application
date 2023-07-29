77c115ac6025f4e6774c66b2d4b5a5b1d59aa144
Sandas Gimhana <109182524+sandazz@users.noreply.github.com>
2023-07-30 02:58:40 +0530

    add validation and customSnackBars - add validation and customSnackBars to collector plastic/cane/glass adding - add validation and customSnackBars to login - add validation and customSnackBars to registration

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -50,23 +50,24 @@ class _loginState extends State<login> with WidgetsBindingObserver {
     if (value == null || value.isEmpty) {
       return 'Email is required';
     }
+
     final emailRegExp = RegExp(r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$');
     if (!emailRegExp.hasMatch(value)) {
       return 'Enter a valid email address';
     }
+
     return null;
   }
+
 
   String? _validatePassword(String? value) {
     if (value == null || value.isEmpty) {
       return 'Password is required';
     }
-    if (value.length < 8) {
-      return 'Password must be at least 8 characters long';
-    }
-    // You can add more password validation rules here
     return null;
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -91,7 +92,7 @@ class _loginState extends State<login> with WidgetsBindingObserver {
               child: Container(
                   margin: EdgeInsets.only(top: containerPosition, bottom: 20),
                   width: MediaQuery.of(context).size.width - 50, // Set the width of the container
-                  height: MediaQuery.of(context).size.height*0.34, // Set the height of the container
+                  // Set the height of the container
                   decoration: BoxDecoration(
                     color: Colors.white, // Set the background color of the container
                     borderRadius: BorderRadius.circular(15.0), // Set the border radius of the container
@@ -204,21 +205,24 @@ class _loginState extends State<login> with WidgetsBindingObserver {
                               ),
                               child: Text('Sign In'),
                             ),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => Register()), // Replace SecondPage with your desired destination page
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                minimumSize: Size(100.0, 40.0),
-                                primary: Colors.green[700], // Set the desired button color
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15.0), // Set the desired border radius
+                            Padding(
+                              padding:  EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => Register()), // Replace SecondPage with your desired destination page
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(100.0, 40.0),
+                                  primary: Colors.green[700], // Set the desired button color
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0), // Set the desired border radius
+                                  ),
                                 ),
+                                child: Text('Sign up'),
                               ),
-                              child: Text('Sign up'),
                             ),
                           ],
                         ),
