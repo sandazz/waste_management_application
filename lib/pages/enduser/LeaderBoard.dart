56c1418332adc09bca81cf3941f76faefa545b8d
Sandas Gimhana <109182524+sandazz@users.noreply.github.com>
2023-09-04 11:27:10 +0530

    add descriptions - added descriptions to all front end items to identify

--- lib/pages/enduser/LeaderBoard.dart
+++ lib/pages/enduser/LeaderBoard.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 
 class LeaderBoard extends StatefulWidget {
@@ -11,6 +12,9 @@ class LeaderBoard extends StatefulWidget {
 
 class _LeaderBoardState extends State<LeaderBoard> {
   @override
+
+  //showcase
+  final GlobalKey _one = GlobalKey();
 
   Future<void> getRecycleUsers() async {
     try {
@@ -63,13 +67,20 @@ class _LeaderBoardState extends State<LeaderBoard> {
   List<double> redeemablePoints = [
   ];
 
+  void showCaseCall(){
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(Duration(milliseconds: 500), () {
+        ShowCaseWidget.of(context).startShowCase([_one]);
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     getRecycleUsers();
+    showCaseCall();
   }
-
-
 
   Widget build(BuildContext context) {
     int colorIntensity = 800;
@@ -90,69 +101,73 @@ class _LeaderBoardState extends State<LeaderBoard> {
         ),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 10.0),
-          child: Column(
-            children: names.asMap().entries.map((entry) {
-              int idx = entry.key;
-              double points = redeemablePoints[idx];
-              int index = entry.key + 1; // count of the names
-              String text = entry.value; // names in the list
-              String indexWithLeadingZero = index.toString().padLeft(2, '0'); // to make numbering 01, 02, 03
+          child: Showcase(
+            key: _one,
+            description: 'Short listed according to the points Users got',
+            child: Column(
+              children: names.asMap().entries.map((entry) {
+                int idx = entry.key;
+                double points = redeemablePoints[idx];
+                int index = entry.key + 1; // count of the names
+                String text = entry.value; // names in the list
+                String indexWithLeadingZero = index.toString().padLeft(2, '0'); // to make numbering 01, 02, 03
 
-              if (colorIntensity > 300){
-                colorIntensity = colorIntensity - (index * 100);
-              }else{
-                colorIntensity =50;
-              }
-              return AspectRatio(
-                aspectRatio: 12/2,
-                  child: Container(
-                      width: 500.0, // Set the width of the container
-                      height: 50.0, // Set the height of the container
-                      padding: EdgeInsets.all(16.0), // Set the padding within the container
-                      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 3.0), // Set the margin around the container
-                      decoration: BoxDecoration(
-                        color: Colors.green[colorIntensity], // Set the background color of the container
-                        borderRadius: BorderRadius.circular(20.0), // Set the border radius of the container
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2.0,
-                            blurRadius: 5.0,
-                            offset: Offset(0, 3), // Adjust the shadow position as needed
-                          ),
-                        ],
-                      ),
-                    child: Row(
-                      children: [
-                        Text(
-                          '$indexWithLeadingZero',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                          ),
+                if (colorIntensity > 300){
+                  colorIntensity = colorIntensity - (index * 100);
+                }else{
+                  colorIntensity =50;
+                }
+                return AspectRatio(
+                  aspectRatio: 12/2,
+                    child: Container(
+                        width: 500.0, // Set the width of the container
+                        height: 50.0, // Set the height of the container
+                        padding: EdgeInsets.all(16.0), // Set the padding within the container
+                        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 3.0), // Set the margin around the container
+                        decoration: BoxDecoration(
+                          color: Colors.green[colorIntensity], // Set the background color of the container
+                          borderRadius: BorderRadius.circular(20.0), // Set the border radius of the container
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2.0,
+                              blurRadius: 5.0,
+                              offset: Offset(0, 3), // Adjust the shadow position as needed
+                            ),
+                          ],
                         ),
-                        Spacer(), // Spacer will take all available space pushing the next widgets to the center
-                        Expanded( // Expanded will ensure the text widget takes only the space it needs
-                          child: Center(
-                            child: Text(
-                              text,
-                              style: TextStyle(
-                                fontSize: 20.0,
+                      child: Row(
+                        children: [
+                          Text(
+                            '$indexWithLeadingZero',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          Spacer(), // Spacer will take all available space pushing the next widgets to the center
+                          Expanded( // Expanded will ensure the text widget takes only the space it needs
+                            child: Center(
+                              child: Text(
+                                text,
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Spacer(), // Spacer will take all available space pushing the next widgets to the end
-                        Text(
-                          '${points}',
-                          style: TextStyle(
-                            fontSize: 20.0,
+                          Spacer(), // Spacer will take all available space pushing the next widgets to the end
+                          Text(
+                            '${points}',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+            ),
           ),
         ),
       ],
